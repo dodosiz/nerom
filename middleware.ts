52e2429009433d97257b07c9585ad51f8e1a8437
Theodosios Asvestopoulos <t.v.asvestopoulos@outlook.com>
2025-03-31 23:35:36 +0300

    Middleware for language detection and fixed critical vulnerabilities

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Lang } from "./app/localization";
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
 
 const locales: Lang[] = ["en", "el"];
+const defaultLocale: Lang = "en";
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
+  const languages = new Negotiator({
+    headers: Object.fromEntries(request.headers),
+  }).languages();
+
+  const lang = match(languages, locales, defaultLocale) as Lang;
   const pathnameHasLocale = locales.some(
     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
   );
 
   if (pathnameHasLocale) return;
 
-  request.nextUrl.pathname = `/en/${pathname}`;
+  request.nextUrl.pathname = `/${lang}/${pathname}`;
   // e.g. incoming request is /products
   // The new URL is now /en-US/products
   return NextResponse.redirect(request.nextUrl);
